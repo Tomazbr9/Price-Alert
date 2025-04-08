1499e651732c0cf59ab279d05eb6fe4e83da61de
Bruno Tomaz <tomazcraft@gmail.com>
2025-04-08 11:41:25 -0300

    Foi criado uma pasta armazenar funçoes uteis

---
+++ util/email.py
@@ -0,0 +1,17 @@
+from django.core.mail import send_mail
+from django.conf import settings
+from decimal import Decimal
+
+def send_email_for_user(
+        name_product: str, 
+        new_product_price: Decimal, 
+        recipient: str) -> None:
+
+    subject: str = 'Preço Alterado!'
+    message: str = f'O preço do produto {name_product} foi alterado para {new_product_price:.2f}!!'
+
+    send_mail(
+        subject,
+        message, 
+        settings.EMAIL_HOST_USER,
+        [recipient], fail_silently=False)
