cb5b7dd336678feaa6b4321903e49ce11c0733b7
Bruno Tomaz <tomazcraft@gmail.com>
2025-04-14 11:18:27 -0300

    Foi criado um model para guardar informaçoes do produtos no banco de dados

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
